2b750bd48e5bb22ec7f70656c87032c58d653628
Neema Grace <neemagrace2002@gmail.com>
2023-08-11 12:45:56 +0200

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react"
 import ClientComponent from "./components/check/Client"
 import Sidebar from "./components/component/Sidebar"
 
@@ -6,7 +6,8 @@ function HomePage() {
   return (
     <div>
       <header><title> Todo app </title></header>
-      <h1>It's Great to have you here!</h1>
+      <h1>It&apos;s Great to have you here!</h1>
+      <p> welcome to your todo app</p>
       {/* Using the client component */}
       (<ClientComponent />)
       <Sidebar/>
