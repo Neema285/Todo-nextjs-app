3fd1e67ba0e7ac29b44c4a8b3ece600479776546
Neema Grace <neemagrace2002@gmail.com>
2023-08-21 16:03:18 +0200

    first commit

---
+++ app/components/TodoLIst.tsx
@@ -0,0 +1,24 @@
+
+export default function TodoLIst() {
+  return (
+    <div className="overflow-x-auto">
+  <table className="table">
+    {/* head */}
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Favorite Color</th>
+      </tr>
+    </thead>
+    <tbody>
+      {/* row 1 */}
+      <tr>
+        
+        <td>Neema Grace</td>
+        <td>Blue</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,22 @@
-import React from "react"
-import ClientComponent from "./components/check/Client"
-import Sidebar from "./components/component/Sidebar"
-
-function HomePage() {
+import AddTask from "./components/addtask";
+import TodoList from "./components/TodoLIst";
+export default function HomePage(){
   return (
-    <div>
-      <header><title> Todo app </title></header>
-      <h1>It&apos;s Great to have you here!</h1>
-      <p> welcome to your todo app</p>
-      {/* Using the client component */}
-      (<ClientComponent />)
-      <Sidebar/>
-     
+    <main className='max-w-4xl max-auto mt-4'>
+    <div className='text-center my-5 flex flex-col gap-4  display-center'>
+
+      <h1 className='text-22xl font-bold'>Todo List</h1>
+      
+      
+      <AddTask/>
+
       </div>
-      )
+      <TodoList/>
+
+      </main>
+      );
 }
 
-export default HomePage;
 
 
 
