2a4a09fd7d081bba931d3d03559d54ff6b4b0fb5
Neema Grace <neemagrace2002@gmail.com>
2023-08-21 16:29:40 +0200

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function HomePage(){
   return (
     <main className='max-w-4xl max-auto mt-4'>
     <div className='text-center my-5 flex flex-col gap-4  display-center'>
-
+      <header><title>TODO APP</title></header>
       <h1 className='text-22xl font-bold'>Todo List</h1>
       
       
